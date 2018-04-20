399311859d4291cee8b33fa9ae64b87950ffb6c7
Pere Urbón <purbon@users.noreply.github.com>
2018-04-20 22:43:37 +0200

    Add better unit testing (#25)
    * add better sintetic test for the fair search algorithm
    
    * code cleanup

--- src/main/java/com/purbon/search/fair/lib/FairTopK.java
+++ src/main/java/com/purbon/search/fair/lib/FairTopK.java
@@ -4,7 +4,9 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.util.PriorityQueue;
 
+import java.util.List;
+
 public interface FairTopK {
 
-    TopDocs fairTopK(PriorityQueue<ScoreDoc> p0, PriorityQueue<ScoreDoc> p1, int k, float p, float alpha);
+    TopDocs fairTopK(List<ScoreDoc> npQueue, List<ScoreDoc> pQueue, int k, float p, float alpha);
 }
